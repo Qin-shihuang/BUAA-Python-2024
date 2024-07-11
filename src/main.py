4439e7d9be86252c9340f81b4b914b5d9e2d0c36
Qin-shihuang <0.0@owo.li>
2024-07-12 00:06:38 +0800

    feat: init project hierarchy

---
+++ src/main.py
@@ -0,0 +1,13 @@
+"""
+Author: Iz0
+Date: 2024-07-12
+License: MIT License
+Description: Entry point of the application
+"""
+
+import frontend.main_window
+
+if __name__ == "__main__":
+    print("Hello, World!")
+    frontend.main_window.main()
+
