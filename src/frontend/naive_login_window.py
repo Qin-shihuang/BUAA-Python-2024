ad19997a81a550c713391cfa221bcb724a005634
Qin-shihuang <0.0@owo.li>
2024-07-12 14:03:33 +0800

    feat: replaced hbox with form & button clicked event

--- src/frontend/naive_login_window.py
+++ src/frontend/naive_login_window.py
@@ -5,31 +5,41 @@ License: MIT License
 Description: Just a test file, NOT FOR PRODUCTION
 """
 
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QFormLayout
 
 class LoginWindowUI(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Login")
-        self.setGeometry(100, 100, 400, 400)
-        layout = QVBoxLayout()
-        username_layout = QHBoxLayout()
-        username_layout.addWidget(QLabel("Username"))
-        username_layout.addWidget(QLineEdit())
-        layout.addLayout(username_layout)
-        password_layout = QHBoxLayout()
-        password_layout.addWidget(QLabel("Password"))
-        password_layout.addWidget(QLineEdit())
-        layout.addLayout(password_layout)
-        layout.addWidget(QPushButton("Login"))
-        self.setLayout(layout)
+        self.layout = QVBoxLayout()
+        
+        self.response_label = QLabel()
+        
+        form_layout = QFormLayout()
+        self.username_input = QLineEdit()
+        self.password_input = QLineEdit()
+
+        self.login_button = QPushButton("Login")
+        form_layout.addRow("Username", self.username_input)
+        form_layout.addRow("Password", self.password_input)
+        self.layout.addLayout(form_layout)
+        self.layout.addWidget(self.response_label)
+        self.layout.addWidget(self.login_button)
+        # TODO: register
+        self.setLayout(self.layout)
 
 class LoginWindow(LoginWindowUI):
+    # TODO: Login logic
     def __init__(self):
         super().__init__()
+        self.login_button.clicked.connect(self.login_button_clicked)
         
     def show(self):
         super().show()
+    
+    def login_button_clicked(self):
+        self.response_label.setStyleSheet("color: red")
+        self.response_label.setText("Login button clicked!")
 
 if __name__ == "__main__":
     app = QApplication([])
