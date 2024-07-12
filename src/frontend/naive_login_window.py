0fba193133a975695380134e68d46caecf81bb46
Qin-shihuang <0.0@owo.li>
2024-07-12 13:12:00 +0800

    fix: fixed vertically placed label and textbox

--- src/frontend/naive_login_window.py
+++ src/frontend/naive_login_window.py
@@ -5,20 +5,24 @@ License: MIT License
 Description: Just a test file, NOT FOR PRODUCTION
 """
 
-from PyQt5.QtWidgets import QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout
 
 class LoginWindowUI(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Login")
         self.setGeometry(100, 100, 400, 400)
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(QLabel("Username"))
-        self.layout.addWidget(QLineEdit())
-        self.layout.addWidget(QLabel("Password"))
-        self.layout.addWidget(QLineEdit())
-        self.layout.addWidget(QPushButton("Login"))
-        self.setLayout(self.layout)
+        layout = QVBoxLayout()
+        username_layout = QHBoxLayout()
+        username_layout.addWidget(QLabel("Username"))
+        username_layout.addWidget(QLineEdit())
+        layout.addLayout(username_layout)
+        password_layout = QHBoxLayout()
+        password_layout.addWidget(QLabel("Password"))
+        password_layout.addWidget(QLineEdit())
+        layout.addLayout(password_layout)
+        layout.addWidget(QPushButton("Login"))
+        self.setLayout(layout)
 
 class LoginWindow(LoginWindowUI):
     def __init__(self):
@@ -26,3 +30,10 @@ class LoginWindow(LoginWindowUI):
         
     def show(self):
         super().show()
+
+if __name__ == "__main__":
+    app = QApplication([])
+    loginwindow = LoginWindow()
+    loginwindow.show()
+    
+    exit(app.exec_())
